2b9f0752c7d97eba187c2030a0838999c0f39848
SideArker <rsowa126@gmail.com>
2023-03-07 12:32:19 +0100

    Changed hexagons to spheres and added some stuff

--- Assets/Scripts/RNGHandler.cs
+++ Assets/Scripts/RNGHandler.cs
@@ -1,45 +1,37 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
 public class RNGHandler : MonoBehaviour
 {
     // Function variables
-    int numCount = 70;
     GameController gameController;
-
     // Lists
     public List<int> numbers = new List<int>();
-    [SerializeField] List<GameObject> hexagons = new List<GameObject>();
-
-
+    [SerializeField] List<GameObject> spheres = new List<GameObject>();
     // Randomizes numbers so that duplicates don't appear
     public void randomize()
     {
         numbers.Clear();
         List<int> allNums = new List<int>();
-        for (int i = 0; i < numCount; i++) allNums.Add(i);
-
-        for (int i = 0; i < numCount; i++)
+        for (int i = 0; i < gameController.numCount; i++) allNums.Add(i);
+        for (int i = 0; i < gameController.numCount; i++)
         {
             int selectedNum = Random.Range(0, allNums.Count);
             numbers.Add(allNums[selectedNum]);
+            // Adds numbers to spheres
+            spheres[i].GetComponentInChildren<TMP_Text>().text = allNums[selectedNum].ToString();
             allNums.Remove(allNums[selectedNum]);
         }
-
-
     }
-
     public void selectNumbers()
     {
         gameController.selectedNumbers.Clear();
-        for(int i = 0; i < gameController.selectionAmount; i++)
+        while (gameController.selectedNumbers.Count < gameController.selectionAmount)
         {
-            int selectedNum = Random.Range(0, numbers.Count);
-            if (!gameController.selectedNumbers.Contains(selectedNum)) gameController.selectedNumbers.Add(selectedNum);
-
+            int selectedIndex = Random.Range(0, numbers.Count);
+            if (!gameController.selectedNumbers.Contains(numbers[selectedIndex])) gameController.selectedNumbers.Add(numbers[selectedIndex]);
         }
     }
-
     void Start()
     {
         gameController = FindObjectOfType<GameController>();
